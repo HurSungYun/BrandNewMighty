61502c1e2db38c7efd658eecaa02f7c663aad03e
Jack Lee <rollerkid02@snu.ac.kr>
2016-07-07 20:38:02 +0900

    RuleManager default

---
+++ Assets/Scripts/RuleManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//TODO: Modify to fit into our project(This is from Syria Game)
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+	private static T _instance;
+	public static GameObject gameController;
+	public static void instantiateSingleton() {
+		_instance = instance;
+	}
+	public static T instance {
+		get {
+			if( _instance == null ) {
+				gameController = GameObject.FindGameObjectWithTag ("GameController");
+				if (gameController == null) {
+					gameController = new GameObject ("GameController");
+					gameController.tag = "GameController";
+					DontDestroyOnLoad (gameController);
+				}
+				_instance = gameController.AddComponent<T> ();
+			}
+			return _instance;
+		}
+	}
+}
+
+
+public class RuleManager : Singleton<RuleManager> {
+
+
+}
