7dfd974a83c58047d5b533922f53a086686a8f74
HurSungYeon <ethan0311@naver.com>
2016-07-18 21:09:02 +0900

    add Card class

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	private bool ableToPlay = false;
+
+	void OnEnable ()  {
+		EventManager.StartListening (this.gameObject.name + "played", PlayFunction);
+	}
+
+	void OnDisable ()  {
+		EventManager.StopListening (this.gameObject.name + "played", PlayFunction);
+	}
+
+	void PlayFunction ()  {
+		Debug.Log (this.gameObject.name + " was played!");
+	}
+
+	void setInfo(int type, int num){
+
+	}
+
+	public void tryPlay(){
+		if (ableToPlay || true) {
+			//TODO: play card
+//			Debug.Log(this.gameObject.name + " played");
+			EventManager.TriggerEvent (this.gameObject.name + "played");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,8 +8,14 @@ public class InputManager : MonoBehaviour {
 
 			var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast (wp, Vector2.zero);
-			if (hit.collider) {
+
+			if (hit.collider) {  //TODO: how we treat other buttons
 				Debug.Log (hit.collider.gameObject.name);
+
+				Card selectedCard = hit.collider.gameObject.GetComponent<Card> ();
+				if (selectedCard != null) {
+					selectedCard.tryPlay ();
+				}
 			}
 		}
 	}
