12c6bdbb3ba00df3586b9a9636aab5b4a28959cf
HurSungYeon <ethan0311@naver.com>
2016-07-18 20:34:14 +0900

    add EventManager & Cards can be clicked

---
+++ Assets/Scripts/EventTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventTest : MonoBehaviour {
+
+	private UnityAction playedListener;
+
+	void Awake () {
+		playedListener = new UnityAction (PlayedFunction);
+	}
+
+	void OnEnable ()  {
+		EventManager.StartListening ("played", playedListener);
+	}
+
+	void OnDisable ()  {
+		EventManager.StopListening ("played", playedListener);
+	}
+
+	void PlayedFunction ()  {
+		Debug.Log ("Some Card was played!");
+	}
+
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+
+			var wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast (wp, Vector2.zero);
+			if (hit.collider) {
+				Debug.Log (hit.collider.gameObject.name);
+			}
+		}
+	}
+}
